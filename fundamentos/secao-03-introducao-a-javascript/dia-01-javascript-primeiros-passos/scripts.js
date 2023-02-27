9cbacfbf07b5ec848ba86c2bbe2c2590a6e62be2
Nina Palma <ninapalmadev@gmail.com>
2023-02-27 19:28:41 -0300

    Altera exercicio com operadores lógicos

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -25,3 +25,33 @@ if (nota >= 80) {
 } else {
     console.log("Infelizmente você reprovou");
 }
+
+/* operadores lógicos*/
+let currentHour = 16;
+let message = "";
+
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir”";
+    console.log(message);
+} else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
+    console.log(message);
+} else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?";
+    console.log(message);
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do almoço!!!";
+    console.log(message);
+} else if (currentHour >= 4 && currentHour < 11) {
+    message = "Hmmm, cheiro de café recém-passado";
+    console.log(message);
+}
+
+
+let weekDay = 'quarta-feira';
+
+if (weekDay == 'segunda-feira' || weekDay == 'terça-feira' || weekDay == 'quarta-feira' || weekDay == 'quinta-feira' || weekDay == 'sexta-feira') {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+    console.log("FINALMENTE, descanso merecido! UwU!");
+}
