a678f4ef005bf910ea16c167e12a4359e7e2132d
Nina Palma <ninapalmadev@gmail.com>
2023-02-27 19:42:40 -0300

    Finaliza treinos  da aula

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -49,9 +49,24 @@ if (currentHour >= 22) {
 
 
 let weekDay = 'quarta-feira';
-
 if (weekDay == 'segunda-feira' || weekDay == 'terça-feira' || weekDay == 'quarta-feira' || weekDay == 'quinta-feira' || weekDay == 'sexta-feira') {
     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
 } else {
     console.log("FINALMENTE, descanso merecido! UwU!");
+}
+
+let statusPessoa = 'aprovada';
+switch (statusPessoa) {
+    case 'aprovada':
+        console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+        break;
+    case 'lista':
+        console.log('Você está na nossa lista de espera.');
+        break;
+    case 'reprovada':
+        console.log('Infelizmente você reprovou');
+        break;
+    default:
+        console.log("Informação incorreta");
+        break;
 }
