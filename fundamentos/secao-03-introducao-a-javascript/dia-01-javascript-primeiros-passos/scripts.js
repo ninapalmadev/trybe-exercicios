26cc7903da42f4482b691c7927e78ded2542a2a8
Nina Palma <ninapalmadev@gmail.com>
2023-02-27 16:39:14 -0300

    Cria exercicio com condicionais

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -6,6 +6,7 @@ console.log(myName);
 console.log(birthCity);
 console.log(bitrthYear);
 
+/* operações */
 let base = 5;
 let height = 8;
 let area = base * height;
@@ -13,3 +14,14 @@ console.log(area);
 let perimeter = base + base + height + height;
 console.log(perimeter);
 
+/* condicionais */
+
+let nota = 85;
+
+if (nota >= 80) {
+    console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");
+} else if (nota <= 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else {
+    console.log("Infelizmente você reprovou");
+}
