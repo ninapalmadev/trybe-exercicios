c2b556e89d582754114fab5115fb9c6e0c403e7b
Nina Palma <ninapalmadev@gmail.com>
2023-02-27 16:30:23 -0300

    Adiciona segundo exercicio

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -5,3 +5,11 @@ let bitrthYear = 1994;
 console.log(myName);
 console.log(birthCity);
 console.log(bitrthYear);
+
+let base = 5;
+let height = 8;
+let area = base * height;
+console.log(area);
+let perimeter = base + base + height + height;
+console.log(perimeter);
+
