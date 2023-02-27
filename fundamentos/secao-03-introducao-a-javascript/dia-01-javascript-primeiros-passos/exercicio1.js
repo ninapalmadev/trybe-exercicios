e93d2a83511aeb2877063e8bd56d6ec997772df4
Nina Palma <ninapalmadev@gmail.com>
2023-02-27 19:49:37 -0300

    Cria exercicio 1 para prática de JS

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio1.js
@@ -0,0 +1,16 @@
+/* parte 1 */
+let a = 3;
+let b = 27;
+
+console.log("Valor de a: " + a + ". Valor de b: " + b);
+console.log("A soma de a + b é: " + (a + b));
+console.log("A subtração de a - b é: " + (a - b));
+console.log("A multiplicação de a X b é: " + (a * b));
+console.log("A divisão de a / b é: " + (a / b));
+console.log("O módulo de a % b é: " + (a % b));
+
+/* parte 2 */
+
+
+
+/* parte 3 */
