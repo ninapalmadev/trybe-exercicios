366ae9c21c8f2cf3ae7e79da58b7b74a049a69d8
Nina Palma <ninapalmadev@gmail.com>
2023-02-28 12:38:30 -0300

    Resolve exercício se é ímpar

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio1.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio1.js
@@ -142,3 +142,9 @@ if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0 ) {
 }
 console.log("-------------------------------------");
 console.log("-------------------------------------");
+
+if (numero1 % 2 !== 0 || numero2 % 2 !== 0 || numero3 % 2 !== 0 ) {
+    console.log(true);
+} else {
+    console.log(false);
+}
