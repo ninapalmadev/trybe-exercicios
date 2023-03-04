b5014d5572a6f4973adf23ffd98372d2d3f3cb89
Nina Palma <ninapalmadev@gmail.com>
2023-03-04 20:14:30 -0300

    cria função para pegar valor da posição passada por parametro

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/bonus.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/bonus.js
@@ -24,26 +24,40 @@ function adicionaTurno(objeto, chave, valor) {
 adicionaTurno(lesson2, 'turno', 'noite')
 console.log(lesson2);
 
-function listaChaves(obj){
+function listaChaves(obj) {
     return Object.keys(obj)
 }
 console.log(listaChaves(lesson3));
 
-function tamanhoObj(obj){
+function tamanhoObj(obj) {
     return Object.keys(obj).length;
 }
 console.log(tamanhoObj(lesson3));
 
-function showValuesObj(obj){
+function showValuesObj(obj) {
     return Object.values(obj);
 }
 console.log(showValuesObj(lesson1));
 
-let allLessons = Object.assign({}, {
-    lesson1: lesson1, 
-    lesson2: lesson2, 
-    lesson3: lesson3
-});
+let allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 // O método "Object.assign()" recebe dois parâmetros: um objeto e o que se quer atribuir à ele. Se no primeiro parâmetro for passado um objeto vazio, ele criará um objeto novo com as caraterísticas do segundo parâmetro.
 
 console.log(allLessons);
+
+function totalStudents(obj) {
+    let total = 0;
+    let keys = Object.keys(obj);
+
+    for (let index in keys) {
+        total += obj[keys[index]].numeroEstudantes;
+    }
+    return total;
+}
+console.log(totalStudents(allLessons));
+
+function getValueByNumber(obj, position){
+    return Object.values(obj)[position];
+}
+
+console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matemática'
