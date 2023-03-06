d2a1f709a3d923cf25a03aa266e27af9d982de85
Nina Palma <ninapalmadev@gmail.com>
2023-03-06 11:47:10 -0300

    Cria uma arrow function

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/script.js
@@ -1,27 +1,46 @@
 // parte 1
 
 function imprimeIdade() {
-    for (let idade = 30; idade <= 40; idade += 1) {
-        console.log('Idade dentro do for:', idade)
-    }
+  for (let idade = 30; idade <= 40; idade += 1) {
+    console.log('Idade dentro do for:', idade)
+  }
 };
 
 imprimeIdade();
 
-  // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
-  const pessoa = {
-    nome: 'Henri',
-    idade: 20
-  }
-
+// Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+const pessoa = {
+  nome: 'Henri',
+  idade: 20
+}
 // pessoa = {
 //     nome: 'Luna',
 //     idade: 19
 // } // Altere essa estrutura para corrigir o erro.
 
-  pessoa.nome = 'Luna';
-  pessoa.idade = 19;
-  
-  console.log('Nome:', pessoa.nome);
-  console.log('Idade:', pessoa.idade);
+pessoa.nome = 'Luna';
+pessoa.idade = 19;
 
+console.log('Nome:', pessoa.nome);
+console.log('Idade:', pessoa.idade);
+
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hambúrguer';
+console.log(favoriteFood);
+
+const name1 = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá, ${name1} ${lastName}!`);
+
+function soma(a, b) {
+  let resultado = a + b;
+  return resultado;
+}
+
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+
+const numeroAleatorio = () =>  Math.random();
+
+console.log(numeroAleatorio());
