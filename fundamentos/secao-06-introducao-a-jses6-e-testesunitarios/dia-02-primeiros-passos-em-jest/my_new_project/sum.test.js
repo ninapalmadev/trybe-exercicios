cd539fb7293cb64344af7ccb9234bad77fefc66b
Nina Palma <ninapalmadev@gmail.com>
2023-03-27 18:39:36 -0300

    Exercicios

--- fundamentos/secao-06-introducao-a-jses6-e-testesunitarios/dia-02-primeiros-passos-em-jest/my_new_project/sum.test.js
+++ fundamentos/secao-06-introducao-a-jses6-e-testesunitarios/dia-02-primeiros-passos-em-jest/my_new_project/sum.test.js
@@ -3,3 +3,14 @@ const sum = (a, b) => a + b;
 test('sums two values', () => {
   expect(sum(2, 3)).toEqual(5);
 });
+
+test('Igualdade de array e object', () => {
+  const arr = [1, 2 ,3];
+  const obj = { a: 1, b: 2, c: 3};
+
+  expect(arr).toBe([1, 2, 3]); // fails
+  expect(obj).toBe({ a: 1, b: 2, c: 3}); // fails
+  expect(arr).toEqual([1, 2, 3]); // OK
+  expect(obj).toEqual({ a: 1, b: 2, c: 3}); // OK
+  
+});
