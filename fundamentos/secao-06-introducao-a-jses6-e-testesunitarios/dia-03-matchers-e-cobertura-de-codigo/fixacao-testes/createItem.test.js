033f56b0c5a433b0927eca886b43e1152adf02c7
Nina Palma <ninapalmadev@gmail.com>
2023-03-28 18:39:16 -0300

    resolve item 4

--- fundamentos/secao-06-introducao-a-jses6-e-testesunitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ fundamentos/secao-06-introducao-a-jses6-e-testesunitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -8,9 +8,22 @@ describe('a função createItem', () => {
       "unit": "kg"
     })
   });
-  it.todo('utiliza zero como quantidade padrão');
-  it.todo('Lança um erro quando não recebe parâmetros');
-  it.todo('Lança um erro se o nome do item não é uma string');
+
+  it('utiliza zero como quantidade padrão', () => {
+    const quantity = 0;
+    expect(createItem('banana', 'kg', 1.99)).toEqual({
+      "name": "banana", 
+      "price": 1.99, 
+      "quantity": quantity, 
+      "unit": "kg"
+    })
+  });
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow();
+  });
+  it('Lança um erro se o nome do item não é uma string', () => {
+    expect(() => createItem(20, '1kg', 1.99, 'banana')).toThrow();
+  });
   it.todo('Lança um erro se o preço é negativo');
   it.todo('Lança um erro se o preço é zero');
 });
