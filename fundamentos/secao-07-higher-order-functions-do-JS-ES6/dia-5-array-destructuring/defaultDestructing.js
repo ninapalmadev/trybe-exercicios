6bd4302ea963481679176ea4499322c92a267e6d
Nina Palma <ninapalmadev@gmail.com>
2023-04-10 15:16:26 -0300

    default destructuring

---
+++ fundamentos/secao-07-higher-order-functions-do-JS-ES6/dia-5-array-destructuring/defaultDestructing.js
@@ -0,0 +1,39 @@
+const position2d = [1.0, 2.0];
+const [x, y, z = 0] = position2d;
+
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // 0
+
+// const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// const person = {
+//   firstName: 'João',
+//   lastName: 'Jr II',
+// };
+
+// const otherPerson = {
+//   firstName: 'Ivan',
+//   lastName: 'Ivanovich',
+//   nationality: 'Russian',
+// };
+
+// console.log(getNationality(otherPerson)); // Ivan is Russian
+// console.log(getNationality(person));
+
+// Passando um valor default para a desestruturação de `nationality` sendo "Brazilian" então todo objeto que passarmos terá esse valor, caso não seja passado algum outro.
+const getNationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person)); // João is Brazilian
