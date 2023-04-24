4c45040b8510616cd376c14ed424857e703a8aee
Nina Palma <ninapalmadev@gmail.com>
2023-04-24 15:26:26 -0300

    resolve requisição com duas API's

--- front-end/introducao-ao-frontend-e-js-assincrono/dia-2-js-assencrono-promises-e-fetch/exercise02/index.js
+++ front-end/introducao-ao-frontend-e-js-assincrono/dia-2-js-assencrono-promises-e-fetch/exercise02/index.js
@@ -7,23 +7,38 @@ const image = document.querySelector('#img');
 
 const DOG_URL = 'https://dog.ceo/api/breeds/image/random';
 
-// dog.addEventListener('click', (event) => {
-//   event.preventDefault();
+dog.addEventListener('click', (event) => {
+  event.preventDefault();
 
-//   fetch(DOG_URL)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       image.src = data.message
-//     })
-//   // console.log(data.message));
-// })
+  fetch(DOG_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      image.src = data.message
+      console.log(data)
+    })
+});
 
 cat.addEventListener('click', (event) => {
   event.preventDefault();
-
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    // .then((data) => console.log(data[0].url))
+    .then((data) => {
+      image.src = data[0].url;
+    });
 })
 
-fetch('https://aws.random.cat/meow')
-  .then((response) => console.log('oi'))
-  // .then((data) => console.log(data.file))
+random.addEventListener("click", () => {
+  Promise.any([
+    fetch("https://aws.random.cat/meow"),
+    fetch("https://api.thecatapi.com/v1/images/search")
+  ])
+    .then((response) => response.json())
+    .then((data) => {
+      image.src = data.message || data[0].url;
+    })
+});
+// fetch('https://api.thecatapi.com/v1/images/search')
+//   .then((response) => console.log('oi'))
+//   // .then((data) => console.log(data.file))
 
