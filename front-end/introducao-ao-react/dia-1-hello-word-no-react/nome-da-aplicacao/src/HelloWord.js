4fbf66ba0aa4646ec60f8dc52ef438156fc08185
Nina Palma <ninapalmadev@gmail.com>
2023-05-02 13:17:52 -0300

    cria projeto de prática da aula 1

---
+++ front-end/introducao-ao-react/dia-1-hello-word-no-react/nome-da-aplicacao/src/HelloWord.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import './App.css';
+
+class HelloWord extends Component {
+  render() {
+    return <h1 className='HelloWord'>Hello, word</h1>
+  }
+}
+
+export default HelloWord;
