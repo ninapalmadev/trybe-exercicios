cf400ea11d36874dc603d41d184068c76b27b2ba
Nina Palma <ninapalmadev@gmail.com>
2023-05-03 20:04:16 -0300

    exercicios com props

---
+++ front-end/introducao-ao-react/dia-2-componentes-react/exercise-listing-components/src/UserProfile.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import Image from './Image';
+
+class UserProfile extends React.Component {
+  render() {
+    const { user } = this.props;
+    return (
+      <div>
+        <p> { user.name } </p>
+        <p> { user.email } </p>
+        <Image source={ user.avatar } alternativeText="User avatar" />
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
