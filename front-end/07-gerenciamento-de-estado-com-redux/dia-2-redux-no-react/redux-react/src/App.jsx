f4c4327dbe026cd47e1e513573e06540e3b9a977
Nina Palma <ninapalmadev@gmail.com>
2023-06-06 11:45:56 -0300

    finaliza exercicio

--- front-end/07-gerenciamento-de-estado-com-redux/dia-2-redux-no-react/redux-react/src/App.jsx
+++ front-end/07-gerenciamento-de-estado-com-redux/dia-2-redux-no-react/redux-react/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
+import React from 'react'
+import { connect } from 'react-redux';
+import { actionCreator } from './redux/actions';
 
-function App() {
-
-  return (
+class App extends React.Component {
+  render() {
+    const { dispatch, countState } = this.props;
+    return (
     <div className="app">
       <h1>Contador</h1>
-      <h2>0</h2>
-    <button>Incrementar</button>
+      <h2>{ countState }</h2>
+      <button onClick={() => dispatch(actionCreator())}>Incrementa 1</button>
+        <button onClick={() => dispatch(actionCreator(5))}>Incrementa 5</button>
     </div>
-      
-  )
+    )
+  }
 }
 
-export default App
+const mapStateToProps = (state) => ({
+  countState: state.count,
+});
+
+export default connect(mapStateToProps)(App);
