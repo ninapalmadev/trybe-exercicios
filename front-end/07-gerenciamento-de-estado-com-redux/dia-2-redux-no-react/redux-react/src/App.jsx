69c0b2dbe6756d48fc25fc3f2a19b22d12861643
Nina Palma <ninapalmadev@gmail.com>
2023-06-06 10:59:12 -0300

    cria arquivos do primeiro exercicio

---
+++ front-end/07-gerenciamento-de-estado-com-redux/dia-2-redux-no-react/redux-react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Contador</h1>
+      <h2>0</h2>
+    <button>Incrementar</button>
+    </div>
+      
+  )
+}
+
+export default App
