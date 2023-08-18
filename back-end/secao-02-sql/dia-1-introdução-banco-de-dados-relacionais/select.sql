9872659082b2b0259e2d810ef6958c33ecddb317
Nina Palma <ninapalmadev@gmail.com>
2023-08-18 13:30:06 -0300

    exercicios de aula

---
+++ back-end/secao-02-sql/dia-1-introdução-banco-de-dados-relacionais/select.sql
@@ -0,0 +1,17 @@
+SELECT 'nina', 'palma', 'sao paulo', 29;
+
+SELECT 'nina' AS Nome, 'palma' AS Sobrenome, 'sao paulo' AS 'Cidade Natal', 29 AS Idade;
+
+SELECT 13 * 8;
+
+SELECT now() as "Data Atual";
+
+SELECT * FROM sakila.city;
+
+SELECT first_name, last_name FROM sakila.customer;
+
+SELECT * FROM sakila.rental;
+
+SELECT title, description, release_year FROM sakila.film;
+
+SELECT * FROM sakila.country;
