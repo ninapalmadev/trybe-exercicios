936211c6b0e687316e3011459859462f1adf8a1c
Nina Palma <ninapalmadev@gmail.com>
2023-08-21 12:46:01 -0300

    arquivos para exercicio

---
+++ back-end/secao-03-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas/strings.sql
@@ -0,0 +1,47 @@
+-- Converte o texto da string para CAIXA ALTA
+SELECT UCASE('Oi, eu sou uma string');
+
+-- Converte o texto da string para caixa  baixa
+SELECT LCASE('Oi, eu sou uma string');
+
+-- Substitui as ocorrências de uma substring em uma string
+SELECT REPLACE('Oi, eu sou uma string', 'string', 'cadeia de caracteres');
+
+-- Retorna a parte da esquerda de uma string de acordo com o
+-- número de caracteres especificado
+SELECT LEFT('Oi, eu sou uma string', 3);
+
+-- Retorna a parte da direita de uma string de acordo com o
+-- número de caracteres especificado
+SELECT RIGHT('Oi, eu sou uma string', 6);
+
+-- Exibe o tamanho, em caracteres, da string, a função LENGTH retorna o tamanho em bytes
+SELECT CHAR_LENGTH('Oi, eu sou uma string');
+
+-- Extrai parte de uma string de acordo com o índice de um caractere inicial
+-- e a quantidade de caracteres a extrair
+SELECT SUBSTRING('Oi, eu sou uma string', 5, 2);
+
+-- Se a quantidade de caracteres a extrair não for definida,
+-- então a string será extraída do índice inicial definido, até o seu final
+SELECT SUBSTRING('Oi, eu sou uma string', 5);
+
+SELECT UCASE(title) FROM sakila.film LIMIT 10;
+SELECT LCASE(title) FROM sakila.film LIMIT 10;
+SELECT REPLACE(title, 'ACADEMY', 'FOO') FROM sakila.film WHERE film_id = 1;
+SELECT LEFT(title, 7) FROM sakila.film WHERE film_id = 1;
+SELECT RIGHT(title, 8) FROM sakila.film WHERE film_id = 1;
+SELECT CHAR_LENGTH(title) FROM sakila.film WHERE film_id = 1;
+SELECT SUBSTRING(title, 5, 2) FROM sakila.film WHERE film_id = 1;
+SELECT SUBSTRING(title, 5) FROM sakila.film WHERE film_id = 1;
+
+SELECT UCASE('Trybe');
+SELECT REPLACE('A Internet mudou o mundo', 'Internet', 'IA');
+SELECT CHAR_LENGTH('Uma frase qualquer');
+SELECT LENGTH('Uma frase qualquer');
+
+SELECT LCASE('RUA NORTE 1500, RIO DE JANEIRO, BRASIL');
+
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13, 10);
+-- OU 
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', -35, 10);
