d2b0afab6ca944d3454812afd1c12b023cbbbb20
Nina Palma <ninapalmadev@gmail.com>
2023-09-22 13:36:16 -0300

    exercicios em aula

---
+++ back-end/secao-06-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-db/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,18 @@
+// src/models/user.model.js
+
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phoneNum: DataTypes.STRING,
+  });
+
+  (async () => {
+    await sequelize.sync({ force: true });
+    // As funções vão aqui
+})();
+
+  return User;
+};
+
+module.exports = UserModel;
