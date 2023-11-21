4b3fdd5c2fe6d53e0f4bb68141ea8e50fe685c70
Nina Palma <ninapalmadev@gmail.com>
2023-11-21 11:11:17 -0300

    inicia exercicios de ciencia da comp

---
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/exercicios.py
@@ -0,0 +1,46 @@
+multiplicacao = 2 * 3  # saída: 6
+soma = 2 + 3  # saída: 5
+divisao = 3 / 2  # saída: 1.5
+subtracao = 3 - 2  # saída: 1
+
+print(multiplicacao + soma + divisao + subtracao)  # saída: 13.5
+
+counter = 0
+
+# original
+counter = counter + 1
+
+# simplificado
+counter += 1
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+a == b  # saída: True
+
+18 < temperature < 25  # saída: True
+
+idade <= 5 or idade >= 65
+
+a = 10
+b = 5
+a + b
+a - b
+a * b
+a / b
+a // b
+a ** b
+a % b
+
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+minutes
+seconds
+
+books = 60
+book_price = (1 - 0.4) * 24.20
+logistic = 3 + (books - 1) * 0.75
+cost = books * book_price + logistic
+cost
+
