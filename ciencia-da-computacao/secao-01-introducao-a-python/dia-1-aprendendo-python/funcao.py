22d3a6cac99dd7e0ac6eda735db091894682cd55
Nina Palma <ninapalmadev@gmail.com>
2023-11-21 12:08:50 -0300

    mais exercicios

--- ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/funcao.py
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/funcao.py
@@ -4,3 +4,27 @@ def soma(x, y):
 soma(2, 2)  # os parâmetros aqui são posicionais
 
 soma(x=2, y=2)  # aqui estamos nomeando os parâmetros
+
+def concat_like_tuple(*args):
+    final_string = ""
+    for index, name in enumerate(args, 1):
+        final_string += f"O nome da pessoa {index} é {name}.\n"
+    return final_string
+
+
+def concat_like_dict(**kwargs):
+    final_string = (
+        f'{kwargs["nome"]} {kwargs["sobrenome"]} tem {kwargs["idade"]} anos.\n'
+    )
+    return final_string
+
+
+concat_like_tuple("Cris", "Wallace", "Carol")
+# saída:
+# O nome da pessoa 1 é Cris.
+# O nome da pessoa 2 é Wallace.
+# O nome da pessoa 3 é Carol.
+
+concat_like_dict(nome="Felipe", sobrenome="Silva", idade=25)
+# saída:
+# Felipe Silva tem 25 anos.
